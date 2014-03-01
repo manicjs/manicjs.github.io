f9b3d4332c04ec5ce3c8b8c12f9f85a16d2b5cda
MrIsaacs <ivan.ilic@gnius.at>
2014-03-01 18:04:16 +0100

    added article controller for main stories

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 var mri = angular.module( 'mrisaacs', [] )
   .config( function( $routeProvider, $locationProvider, $interpolateProvider ) {
-    $routeProvider.when( '/' , {
-      templateUrl : 'views/',
-      controller  : 'AppCtrl'
+ //   $routeProvider.when( '/' , {
+ //     templateUrl : 'html/',
+ //     controller  : 'Ctrl'
     });
   });
+
+mri.controller('MainArticleCtrl', function( $scope ) {
+    $scope.main.title = 'Latest Story';
+}
