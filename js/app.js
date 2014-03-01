bbee31435c91d6a91664e6ab1c785e3e36dfd70e
MrIsaacs <ivan.ilic@gnius.at>
2014-03-01 19:44:46 +0100

    added call method run

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var mri = angular.module( 'mrisaacs', ['ngRoute'] )
         $routeProvider.when( '/' , {
             templateUrl : 'html/main-article.html',
             controller  : 'MainArticleCtrl'
-    });
+    }).run();
 });
 
 mri.controller('MainArticleCtrl', function( $scope ) {
