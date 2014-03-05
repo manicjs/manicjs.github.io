a6fa85ce5398b2a89438aecd8aaafe128ac2efc4
MrIsaacs <ivan.ilic@gnius.at>
2014-03-05 21:35:10 +0100

    added some provider to module

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var mri = angular.module( 'mrisaacs', ['ngRoute'] )
-    .config( function( $routeProvider ) {
+    .config( function( $routeProvider, $locationProvider, $interpolateProvider, $httpProvider ) {
+        $httpProvider.defaults.headers.get['Content-Type'] = 'application/json';
+
         $routeProvider.when( '/' , {
             templateUrl : 'html/main-article.html',
             controller  : 'MainArticleCtrl'
         });
+
+        $locationProvider
+            .html5Mode( true )
+            .hashPrefix( '!' );
+
+        $interpolateProvider.startSymbol( '{[{' );
+        $interpolateProvider.endSymbol( '}]}' );
 });
 
 mri.controller('MainArticleCtrl', function( $scope, $http ) {
