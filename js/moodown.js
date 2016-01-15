51b580eecdb57f8dec14d56368346c215a6ba1f7
Ivan Ilic <ivan.ilic@gnius.at>
2016-01-15 18:20:25 +0100

    few page improvements

---
+++ js/moodown.js
@@ -0,0 +1,27 @@
+/**
+ * Created by MrIsaacs on 14.01.2016.
+ */
+
+var MooDown = new Class ({
+    Implements          : Options,
+
+    markdown            : null,
+
+    options : {
+        markdown    : null // The class for markdown text element
+    },
+
+    moodown : function(element, text){
+        var converter = new Showdown.converter();
+        var html = converter.makeHtml(text);
+
+        $(element).set('html', html);
+    },
+
+    initialize : function(element, options) {
+        var that = this;
+
+        this.setOptions(options);
+        this.moodown(element, this.options.markdown);
+    }
+});
