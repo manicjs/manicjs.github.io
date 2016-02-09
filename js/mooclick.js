c51a42b3a33eaaa37ef5b5d3e385fc5fb0f244f9
Ivan Ilic <ivan.ilic@gnius.at>
2016-02-09 19:44:57 +0100

    few changes for getting the router to work
    feat: add Navigo to library and loads when the DOM is ready
    feat: change avatar-wrapper to manic-logo
    feat: add mooclick to library
    fix: change page description
    fix: add css-style to circled list style in body

---
+++ js/mooclick.js
@@ -0,0 +1,43 @@
+/**
+ * Created by MrIsaacs on 06.02.2016.
+ */
+
+var MooClick = new Class ({
+    Implements : Options,
+
+    elements   : [],
+    _elements  : [],
+    _fn        : false,
+
+    options : {
+        hasData        : false,
+        requestObject  : false
+    },
+
+    initialize : function (elements, options, fn) {
+        this.setOptions(options);
+
+        this._elements = elements;
+        this._fn       = fn;
+
+        this.refresh();
+
+        return this;
+    },
+
+    refresh    : function () {
+        var that = this;
+        this.elements = [];
+
+        Array.each($$(that._elements), function(data) {
+            if (that.options.hasData && data.hasAttribute(('data-' + that.options.hasData))) {
+
+                if (typeof that._fn === 'function') {
+                  data.addEvent('click', that._fn);
+                }
+
+                that.elements.push(data);
+            }
+        });
+    }
+});
