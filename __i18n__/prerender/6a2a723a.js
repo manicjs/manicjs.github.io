c79387bfe3b5965c0c0c7b5ce20d5c5012b514d3
MrIsaacs <MrIsaacs@users.noreply.github.com>
2023-07-02 13:40:55 +0000

    deploy: a35d4c1e6ecd5aeed3320abbed1fdeae1a20f2f3

---
+++ __i18n__/prerender/6a2a723a.js
@@ -0,0 +1,9 @@
+export default {
+  "en": {
+  "and": (ctx) => {const { normalize: _normalize } = ctx;return _normalize(["and"])},
+  "created": (ctx) => {const { normalize: _normalize } = ctx;return _normalize(["created"])},
+  "get-started": (ctx) => {const { normalize: _normalize } = ctx;return _normalize(["Get started"])},
+  "home": (ctx) => {const { normalize: _normalize } = ctx;return _normalize(["Home"])},
+  "updated": (ctx) => {const { normalize: _normalize } = ctx;return _normalize(["updated"])}
+},
+}
